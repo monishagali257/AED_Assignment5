f5b9f57d03c55fb3f53b3fe2e6f80374e6bc68f8
Monishagali <monishagali@users.noreply.github.com>
2021-11-18 10:15:26 -0500

    First commit

---
+++ src/Business/Restaurant/Restaurant.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+import Business.Organization;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harold
+ */
+public class Restaurant {
+           private String Resname;
+
+    Restaurant(String Name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getResname() {
+        return Resname;
+    }
+
+    public void setResname(String Resname) {
+        this.Resname = Resname;
+    }
+          
+    
+
+   
+    
+}
